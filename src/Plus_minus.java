e2a3b0e34e3126218623ac3283ce3043a24733f9
Mir Mahfuz <mirmahfuz99@gmail.com>
2018-11-23 20:37:21 +0600

    update

--- src/Plus_minus.java
+++ src/Plus_minus.java
@@ -1,5 +1,6 @@
+//Given an array of integers, calculate the fractions of its elements that are positive, negative, 
+//and are zeros. Print the decimal value of each fraction on a new line.
 import java.util.*;
-
 public class Plus_minus {
 
 	public static void main(String[] args) {
